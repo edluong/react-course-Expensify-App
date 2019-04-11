36673bbd8047288b958267352b5142143c2ff0e1
Ed <edmundluong@gmail.com>
2019-04-10 20:35:41 -0700

    dispatching redux actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,9 +2,51 @@ import { createStore } from 'redux';
 
 // set up a state container
 // state has no default so set count to 0 inside the function
-const store = createStore((state = { count: 0 }) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    
+    // by convention use a switch statement for actions
+    switch (action.type) {
+        case 'INCREMENT': 
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return{
+                count: 0
+            };
+        default:
+            return state;
+    }
 });
 
 //return the current state obj
+console.log(store.getState());
+
+//Actions
+//dispatch re-runs the redux store
+store.dispatch(
+    //convention for Actions to capitalize 
+    {
+    type: 'INCREMENT' 
+});
+
+store.dispatch(
+    {
+    type: 'INCREMENT' 
+});
+
+store.dispatch({
+    type: 'RESET'
+});
+
+store.dispatch(
+    {
+    type: 'DECREMENT' 
+});
+
+
 console.log(store.getState());
