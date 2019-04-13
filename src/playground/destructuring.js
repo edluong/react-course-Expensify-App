909d5b33c4f488b88a235533224e601379406956
Ed <edmundluong@gmail.com>
2019-04-13 10:59:57 -0700

    array desturucturing

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,34 +1,54 @@
-const person = {
-    name: 'Ed',
-    age: 27,
-    location: {
-        city: 'Philadelphia',
-        temp: 67
-    }
-};
+//object destructuring
 
-// setting default for name
-const {name: firstName = 'Anonymous', age} = person;
-// const name = person.name;
-// const age = person.age;
+// const person = {
+//     name: 'Ed',
+//     age: 27,
+//     location: {
+//         city: 'Philadelphia',
+//         temp: 67
+//     }
+// };
 
-console.log(`${firstName} is ${age}.`);
+// // setting default for name
+// const {name: firstName = 'Anonymous', age} = person;
+// // const name = person.name;
+// // const age = person.age;
 
-// temp property from person.location and rename it to temperature
-const {city, temp: temperature} = person.location
+// console.log(`${firstName} is ${age}.`);
 
-if(city && temperature){
-    console.log(`It's ${temperature} in ${city}.`);
-}
+// // temp property from person.location and rename it to temperature
+// const {city, temp: temperature} = person.location
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// if(city && temperature){
+//     console.log(`It's ${temperature} in ${city}.`);
+// }
 
-const {publisher: publisherName = 'Self-Published'} = book;
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-console.log(publisherName);
+// const {publisher: publisherName = 'Self-Published'} = book;
+
+// console.log(publisherName);
+
+// Array destructuring
+
+const address = ['1299 S Juniper Street','Philadelphia','Pennsylvania','19147'];
+
+// matching by position
+const [street, city, state = 'New York', zip] = address;
+
+// leave a space for the variables you dont want to destructure
+// const [, , state, ] = address;
+
+console.log(`You are in ${city} ${street}.`);
+
+const item = ['Coffee (hot)', '$2.00','$2.50','$2.75'];
+
+const [coffeeType,,mediumPrice] = item;
+
+console.log(`A medium ${coffeeType} costs ${mediumPrice}`);
