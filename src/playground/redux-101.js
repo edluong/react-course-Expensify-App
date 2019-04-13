6fc2ce66c8593c31b8a308b98c2e6e700616a323
Ed <edmundluong@gmail.com>
2019-04-12 23:20:18 -0700

    subscribing and dynamic actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -7,13 +7,20 @@ const store = createStore((state = { count: 0 }, action) => {
     // by convention use a switch statement for actions
     switch (action.type) {
         case 'INCREMENT': 
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            // check if decrementBy is a number then use it, else use default by 1
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
+        case 'SET':
+            return {
+                count: action.count
+            }
         case 'RESET':
             return{
                 count: 0
@@ -23,16 +30,26 @@ const store = createStore((state = { count: 0 }, action) => {
     }
 });
 
-//return the current state obj
-console.log(store.getState());
+// see when a store changes
+const unsubscribe = store.subscribe( () => {
+    //return the current state obj
+    console.log(store.getState());
+});
+
+
+
 
 //Actions
 //dispatch re-runs the redux store
 store.dispatch(
     //convention for Actions to capitalize 
     {
-    type: 'INCREMENT' 
+    type: 'INCREMENT',
+    incrementBy: 5 
 });
+
+// // unsubscribe from the changes
+// unsubscribe();
 
 store.dispatch(
     {
@@ -48,5 +65,12 @@ store.dispatch(
     type: 'DECREMENT' 
 });
 
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
 
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
