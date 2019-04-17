5c95a89fa0812ee54377a676f879486728f78c31
Ed <edmundluong@gmail.com>
2019-04-16 19:11:29 -0700

    finishing up action generators and reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -37,9 +37,23 @@ const setTextFilter = (text = '') => ({
     text
 });
 // SORT_BY_DATE
+const sortbyDate = () => ({
+    type: 'SORT_BY_DATE'
+});
 // SORT_BY_AMOUNT
+const sortbyAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
 // SET_START_DATE
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
 // SET_END_DATE
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
 
 // Expenses Reducer
 const expensesReducerDefaultState = [];
@@ -82,6 +96,26 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
                 ...state,
                 text: action.text
             };
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            };
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            };
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            }
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            }
         default:
             return state;
     }
@@ -107,7 +141,15 @@ store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
 store.dispatch(setTextFilter('rent'));
-store.dispatch(setTextFilter(''));
+store.dispatch(setTextFilter());
+
+store.dispatch(sortbyAmount());
+store.dispatch(sortbyDate());
+
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate(1250));
+
 
 const demoState = {
     expenses: [{
