b4fc6ef6369932894dd5927db3afee0a03f218a0
Ed <edmundluong@gmail.com>
2019-04-17 21:06:03 -0700

    adding expenses to the screen

--- src/app.js
+++ src/app.js
@@ -18,13 +18,13 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-store.dispatch(addExpense({description: 'Water bill' }));
+store.dispatch(addExpense({description: 'Water bill',amount: 4500 }));
 store.dispatch(addExpense({description: 'Gas bill' }));
 
 store.dispatch(setTextFilter('water'));
 
 setTimeout(() => {
-    store.dispatch(setTextFilter('rent'));
+    store.dispatch(setTextFilter('bill'));
 },3000);
 
 const jsx = (
