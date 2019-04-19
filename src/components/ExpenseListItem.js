42945a8f3e2001d1d836c23b1cb9267b4f764a17
Ed <edmundluong@gmail.com>
2019-04-18 23:56:43 -0700

    adding input for filters

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({description, amount, createdAt}) => (
+
+const ExpenseListItem = ({dispatch, description, amount, createdAt, id}) => (
     <div>
         <h3>{description}</h3>
         <p> {amount} - {createdAt}</p>
+        <button onClick={() => {
+            dispatch(removeExpense({ id })); //part of the store context; which is why expense has dispatch
+        }}>Remove</button>
     </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
