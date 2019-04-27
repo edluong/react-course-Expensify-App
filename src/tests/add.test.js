c0c375410b0e53c60dca61cd0a9feed26ec3c26a
Ed <edmundluong@gmail.com>
2019-04-27 16:14:01 -0700

    finished initial jest tests

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,5 +1,19 @@
 const add = (a,b) => a + b;
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`;
+
+
 
 test('should add two numbers', () => {
    const result = add(3,4); 
+   expect(result).toBe(7);  //like assert in python
+});
+
+test('should display Ed', () => {
+   const result = generateGreeting('Ed');
+   expect(result).toBe('Hello Ed!');
+});
+
+test('should generate greeting for no name', () => {
+   const result = generateGreeting();
+   expect(result).toBe('Hello Anonymous!');
 });
